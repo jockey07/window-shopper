6d346433779264288e741f3d9a5a1e861d30594a
Jockey Wilson <jockey_wilson@mac.com>
2018-12-27 22:08:57 -0800

    assests added

---
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Jockey Wilson on 2018-12-27.
+//  Copyright © 2018 Jockey Wilson. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
