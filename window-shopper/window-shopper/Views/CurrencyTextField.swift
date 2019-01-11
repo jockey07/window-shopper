c09c4b2e74c2c65e781eeb091cabcf474c6aa1c8
Jockey Wilson <jockey_wilson@mac.com>
2019-01-10 17:18:06 -0800

    images added

---
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Jockey Wilson on 2019-01-10.
+//  Copyright © 2019 Jockey Wilson. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    
+}
